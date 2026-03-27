91051d7da11fedd0bd85454d52177e28985357f9
Michel <mcugnolio@ik.me>
2026-03-27 08:39:01 +0100

    retour msg3

--- themes/minimalbs/assets/js/contact.js
+++ themes/minimalbs/assets/js/contact.js
@@ -4,8 +4,14 @@ if (contactForm) {
     contactForm.addEventListener('submit', async (e) => {
         e.preventDefault();
 
+        // --- LOGIQUE HONEYPOT ---
+        const honey = document.getElementById('honeypot').value;
+        if (honey !== "") {
+            console.warn("Spam détecté !");
+            return; // On arrête tout sans envoyer au Worker
+        }
+
         const msgDiv = document.getElementById('response-msg');
-        // On cible le bouton à l'intérieur du formulaire
         const submitBtn = contactForm.querySelector('button');
 
         msgDiv.innerText = "Envoi en cours...";
@@ -25,21 +31,15 @@ if (contactForm) {
             });
 
             if (response.ok) {
-                // SUCCESS
                 msgDiv.textContent = "Merci pour votre message, nous vous répondrons au plus tôt.";
                 msgDiv.className = "contact-success-msg";
-
-                // On cache le bouton d'envoi
                 if (submitBtn) submitBtn.classList.add('hidden');
-
                 contactForm.reset();
             } else {
-                // ERROR SERVEUR (ex: 500)
                 msgDiv.textContent = "Erreur lors de l'envoi. Réessayez plus tard.";
                 msgDiv.className = "contact-error-msg";
             }
         } catch (error) {
-            // ERROR RESEAU
             msgDiv.textContent = "Impossible de joindre le serveur.";
             msgDiv.className = "contact-error-msg";
         }
